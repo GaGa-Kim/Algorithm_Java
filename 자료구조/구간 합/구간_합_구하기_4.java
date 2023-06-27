2cc127e5f080331c1a4ff364580e5ffdab9b423b
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-06-27 13:12:48 +0900

    자료구조

---
+++ 자료구조/구간 합/구간_합_구하기_4.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 11659) 구간_합_구하기_4
+ */
+public class 구간_합_구하기_4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        // suNo(숫자 개수), quizNo(질의 개수) 저장하기
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        // 숫자 개수만큼 반복하기
+        for (int i = 1; i <= suNo; i++) {
+            // 합 배열 생성하기(S[i] = S[i - 1] + A[i])
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        // 질의 개수만큼 반복하기
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            // 질의 범위 받기(i ~ j)
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            // 구간 합 출력하기(S[j] - S[i - 1])
+            System.out.println(S[j] - S[i - 1]);
+        }
+    }
+
+}
---
+++ 자료구조/배열과 리스트/숫자의_합.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * 11720) 숫자의_합
+ */
+public class 숫자의_합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // N값 입력받기
+        int N = sc.nextInt();
+        // 길이 N의 숫자를 입력받아 String형 변수 sNum에 저장하기
+        String sNum = sc.next();
+        // sNum을 다시 char[]형 변수 cNum에 변환하여 저장하기
+        char[] cNum = sNum.toCharArray();
+        // int형 변수 sum 선언하기
+        int sum = 0;
+        // cNum 길이만큼 반복하기
+        for (int i = 0; i < cNum.length; i++) {
+            // 배열의 각 자릿값을 아스키코드를 이용하여 정수형으로 변환하며 sum에 더하여 누적하기
+            sum += cNum[i] - '0';
+        }
+        // sum 출력하기
+        System.out.print(sum);
+    }
+}
