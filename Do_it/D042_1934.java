f8bdd6893b40a150983843e9a58383bab4b59ca9
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-09-16 13:17:49 +0900

    유클리드 호제법

---
+++ Do_it/D042_1934.java
@@ -0,0 +1,35 @@
+package Do_it;
+
+import java.util.Scanner;
+
+/**
+ * 1934) 최소공배수
+ */
+public class D042_1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // t(테스트 케이스)
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            // a(1번째 수)
+            int a = sc.nextInt();
+            // b(2번째 수)
+            int b = sc.nextInt();
+            // 결괏값 = a * b / gcd(a, b)
+            int result = a * b / gcd(a, b);
+            // 결괏값 출력하기
+            System.out.println(result);
+        }
+    }
+
+    // 최대 공약수 함수 구현하기
+    public static int gcd(int a, int b) {
+        // if(b가 0이면)
+        if (b == 0)
+            // a가 최대 공약수
+            return a;
+        else
+            // gcd(작은 수, 큰 수 % 작은 수)
+            return gcd(b, a % b);
+    }
+}
