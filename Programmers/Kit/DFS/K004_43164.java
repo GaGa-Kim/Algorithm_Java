4b7a340f4893be302b18f68b8a38cdc775333b2c
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-12-05 17:23:08 +0900

    231205

---
+++ Programmers/Kit/DFS/K004_43164.java
@@ -0,0 +1,65 @@
+package Programmers.Kit.DFS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 43164) 여행경로
+ */
+public class K004_43164 {
+    static ArrayList<String> allRoute;
+    static boolean[] visited;
+
+    // tickets(항공권 정보가 담긴 2차원 배열)
+    public String[] solution(String[][] tickets) {
+        // answer(방문하는 공항 경로 배열)
+        String[] answer = {};
+        // allRoute(방문하는 모든 공항 경로 리스트)
+        allRoute = new ArrayList<>();
+        // visited(항공권 사용 유무 배열)
+        visited = new boolean[tickets.length];
+        // visitCount(사용한 항공권 수)
+        int visitCount = 0;
+        // 가능한 모든 경로 탐색
+        dfs("ICN", "ICN", tickets, visitCount);
+        // allRoute 정렬
+        Collections.sort(allRoute);
+        // " "으로 분리하여 저장
+        answer = allRoute.get(0).split(" ");
+        // answer 반환
+        return answer;
+    }
+
+    private void dfs(String start, String route, String[][] tickets, int visitCount) {
+        // 모든 항공권을 사용했다면
+        if (visitCount == tickets.length) {
+            // 현재 경로를 모든 공항 경로 리스트에 저장
+            allRoute.add(route);
+            return;
+        }
+        for (int i = 0; i < tickets.length; i++) {
+            // tickets[i][0]이 항공권 출발지와 같으며 항공권 i를 사용하지 않았다면
+            if (start.equals(tickets[i][0]) && visited[i] != true) {
+                // 현재 항공권 사용 처리 갱신
+                visited[i] = true;
+                // 현재 항공권의 도착지를 출발지로 변경한 후 경로 탐색
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, visitCount + 1);
+                // 현재 항공권 사용 초기화
+                visited[i] = false;
+            }
+        }
+    }
+
+    // 테스트 케이스
+    public static void main(String[] args) {
+        K004_43164 solution = new K004_43164();
+
+        String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },
+                { "ATL", "SFO" } };
+
+        String[] result = solution.solution(tickets);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
