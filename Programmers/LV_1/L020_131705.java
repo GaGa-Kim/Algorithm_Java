93cc2f54a88b824b477137e2fd8bc04c5dbb6282
Gagyeong Kim <wn8925@sookmyung.ac.kr>
2023-12-15 13:10:30 +0900

    231215

---
+++ Programmers/LV_1/L020_131705.java
@@ -0,0 +1,55 @@
+package Programmers.LV_1;
+
+/**
+ * 131705) 삼총사
+ */
+public class L020_131705 {
+    // answer(학생들 중 삼총사를 만들 수 있는 방법의 수)
+    static int answer;
+    // temp(세 명의 학생들의 번호를 가지는 임시 정수 배열)
+    static int[] temp = new int[3];
+
+    // number(한국중학교 학생들의 번호를 나타내는 정수 배열)
+    public int solution(int[] number) {
+        answer = 0;
+        // DFS(0, 0, number)
+        DFS(0, 0, number);
+        // answer 반환
+        return answer;
+    }
+
+    // DFS
+    private void DFS(int start, int depth, int[] number) {
+        // DFS의 깊이가 3이라면
+        if (depth == 3) {
+            // sum(번호들의 합)
+            int sum = 0;
+            for (int i = 0; i < 3; i++) {
+                // sum에 temp[i] 합하기
+                sum += temp[i];
+            }
+            // sum이 0이라면
+            if (sum == 0)
+                // answer 증가
+                answer++;
+            return;
+        }
+        for (int i = start; i < number.length; i++) {
+            // 임시 정수 배열에 현재 번호 저장
+            temp[depth] = number[i];
+            // DFS(i + 1, depth + 1) 재귀
+            DFS(i + 1, depth + 1, number);
+        }
+    }
+
+    // 테스트 케이스
+    public static void main(String[] args) {
+        L020_131705 solution = new L020_131705();
+
+        int[] number = { -2, 3, 0, 2, -5 };
+
+        int result = solution.solution(number);
+
+        System.out.println(result);
+    }
+}
---
+++ Programmers/LV_1/L021_131128.java
@@ -0,0 +1,68 @@
+package Programmers.LV_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 131128) 숫자_짝꿍
+ */
+public class L021_131128 {
+    // X, Y(짝꿍을 찾을 두 정수)
+    public String solution(String X, String Y) {
+        // yMap(Y의 값을 저장할 HashMap)
+        Map<String, Integer> yMap = new HashMap<>();
+        // yMap에 Y의 값을 저장
+        for (String s : Y.split("")) {
+            yMap.put(s, yMap.getOrDefault(s, 0) + 1);
+        }
+        // answer(두 정수의 짝꿍)
+        List<String> answer = new ArrayList<>();
+        // X가 yMap에 존재하는지 확인
+        for (String s : X.split("")) {
+            // yMap에 s가 존재하며 s의 개수가 0보다 많다면
+            if (yMap.containsKey(s) && yMap.get(s) > 0) {
+                // answer에 짝꿍 저장
+                answer.add(s);
+                // yMap에서 HashMap의 값을 -1 감소
+                yMap.put(s, yMap.get(s) - 1);
+            }
+        }
+        // answer이 비었다면
+        if (answer.isEmpty()) {
+            // -1 반환
+            return "-1";
+        } else {
+            // answer 내림차순 정렬
+            answer.sort(Collections.reverseOrder());
+            // answer의 첫 번째 값이 0이라면
+            if (answer.get(0).equals("0")) {
+                // 0 반환
+                return "0";
+            } else {
+                // sb(StringBuilder)
+                StringBuilder sb = new StringBuilder();
+                // answer의 값들을 붙여 문자열로 변환
+                for (String s : answer) {
+                    sb.append(s);
+                }
+                // sb 반환
+                return sb.toString();
+            }
+        }
+    }
+
+    // 테스트 케이스
+    public static void main(String[] args) {
+        L021_131128 solution = new L021_131128();
+
+        String X = "5525";
+        String Y = "1255";
+
+        String result = solution.solution(X, Y);
+
+        System.out.println(result);
+    }
+}
